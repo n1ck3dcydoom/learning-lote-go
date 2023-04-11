7edfc484120ef5d9ff45fd823ecd191c02e76445
n1ck3dcydoom <n1ck3dcydoom@outlook.com>
2023-04-11 19:58:33 +0800

    Leetcode 1041. 困于环中的机器人 (模拟)

--- main.go
+++ main.go
@@ -2,29 +2,9 @@ package main
 
 import (
 	"fmt"
-	"learning-lote-go/codetop/common"
-	"learning-lote-go/leetcode/stack"
+	"learning-lote-go/leetcode/simulate"
 )
 
 func main() {
-	head := &common.ListNode{
-		Val: 2,
-		Next: &common.ListNode{
-			Val: 7,
-			Next: &common.ListNode{
-				Val: 4,
-				Next: &common.ListNode{
-					Val: 3,
-					Next: &common.ListNode{
-						Val:  5,
-						Next: nil,
-					},
-				},
-			},
-		},
-	}
-	res := stack.NextLargerNodes(head)
-	for n := range res {
-		fmt.Println(n)
-	}
+	fmt.Println(simulate.IsRobotBounded("GG"))
 }
